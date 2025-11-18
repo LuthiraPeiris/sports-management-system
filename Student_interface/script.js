fe7631e22bdcff27cddd60e086c305f2cc08db1b
Rashmika H.G.K <hgkrashmika@std.foc.sab.ac.lk>
2025-11-18 18:41:21 +0530

    finished JS file

--- Student_interface/script.js
+++ Student_interface/script.js
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 coach: 'Mr. Perera',
                 nextPractice: '3 Nov, 4:00 PM',
                 location: 'University Ground',
-                attendence: '80%',
+                attendance: '80%',
                 link: '#',
                 buttonText: 'Leave Sport'
             },
@@ -66,5 +66,96 @@ document.addEventListener('DOMContentLoaded', () => {
     loadMySports(studentData.sports);
     loadAchievements(studentData.achievements);
 
-    
-})
+    function loadHeaderProfile(fullName, status, imageUrl) {
+        const profileImg = document.getElementById('profile-pic-preview');
+        const profileName = document.getElementById('profile-name');
+        const profileStatus = document.getElementById('profile-status');
+        
+        if (profileImg) {
+            profileImg.src = imageUrl;
+        }
+
+        if (profileName) {
+            profileName.textContent = fullName;
+        }
+
+        if (profileStatus) {
+            profileStatus.textContent = status;
+        }
+
+    }
+
+    function loadWelcomeMassage(name) {
+        const welcomeHeader = document.getElementById('welcome-header');
+        if (welcomeHeader) {
+            welcomeHeader.textContent = `welcome back ${name} !`;
+        }
+    }
+
+    function loadStats(stats) {
+        document.getElementById('stats-enrolled').textContent = stats.enrolled;
+        document.getElementById('stats-registered').textContent = stats.registered;
+        document.getElementById('stats-achievements').textContent = stats.achievements;
+    }
+
+    function loadSchedule(scheduleItems) {
+        const scheduleList = document.getElementById('schedule-list');
+        if (!scheduleList) return;
+
+        scheduleList.innerHTML = '';
+
+        scheduleItems.forEach(item => {
+            const div = document.createElement('div');
+            div.className = 'schedule-item';
+
+            div.innerHTML = `
+                <div class="date">
+                    <span>${item.date}</span>
+                    <small>${item.month}</small>
+                </div>
+                <div class="event-name">${item.event}</div>
+            `;
+
+            scheduleList.appendChild(div);
+        });
+    }
+
+    function loadMySports(sports) {
+        const sportsContainer = document.getElementById('sports-container');
+        if (!sportsContainer) return;
+
+        sportsContainer.innerHTML = '';
+        
+        sports.forEach(sport => {
+            const card = document.createElement('div');
+            card.className = 'sport-card';
+            card.innerHTML = `
+                <h3>${sport.name}</h3>
+                <p>Coach: ${sport.coach}</p>
+                <p>Next Practice: ${sport.nextPractice}</p>
+                <p>Location: ${sport.location}</p>
+                <p>Attendance: ${sport.attendance}</p>
+                <a href="${sport.link}" class="action-button">${sport.buttonText}</a>
+            `;
+            sportsContainer.appendChild(card);
+      });
+    }
+
+    function loadAchievements(achievements) {
+        const achievementsContainer = document.getElementById('achievements-container');
+        if(!achievementsContainer) return;
+
+        achievementsContainer.innerHTML = '';
+
+        achievements.forEach(achievement => {
+            const card = document.createElement('div');
+            card.className = 'achievement-card';
+            card.innerHTML = `
+                <h4>${achievement.title}</h4>
+                <p>Date: ${achievement.date}</p>
+                <p>Event: ${achievement.event}</p>
+            `;
+            achievementsContainer.appendChild(card);
+        });
+    }
+});
